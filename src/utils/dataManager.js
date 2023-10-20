f352d4ca461ca7e8e0bf44b75506a9e5f5cce7a8
ienrum <azang0506@gmail.com>
2023-10-20 13:57:36 +0900

    ✨ : 데이터를 명시적으로 업데이트와 데이터를 명시적으로 추가하는 코드 추가

--- src/utils/dataManager.js
+++ src/utils/dataManager.js
@@ -10,3 +10,55 @@ export function findDocumentFromTree(data, id) {
   });
   return result;
 }
+
+export function updateDocumentFromTree(documents, id, data, callback) {
+  const copiedDocuments = JSON.parse(JSON.stringify(documents));
+  if (id === "null") {
+    copiedDocuments.push(data);
+    return copiedDocuments;
+  }
+  copiedDocuments.forEach((item) => {
+    if (item.id === Number(id)) {
+      callback(item, data);
+      return copiedDocuments;
+    } else if (item.documents.length > 0) {
+      const children = updateDocumentFromTree(
+        item.documents,
+        id,
+        data,
+        callback
+      );
+      if (children) {
+        item.documents = children;
+      }
+    }
+  });
+  return copiedDocuments;
+}
+export function updateEditingPostToSideBar(sideBarData, editingPostData) {
+  const copiedSideBarData = JSON.parse(JSON.stringify(sideBarData));
+  const nextState = updateDocumentFromTree(
+    copiedSideBarData,
+    editingPostData.id,
+    editingPostData,
+    (item, data) => {
+      item.title = data.title;
+    }
+  );
+  return nextState;
+}
+
+export function appendEditingPostToSideBar(sideBarData, postData, parentId) {
+  postData.documents = [];
+  const copiedSideBarData = JSON.parse(JSON.stringify(sideBarData));
+  const nextState = updateDocumentFromTree(
+    copiedSideBarData,
+    parentId,
+    postData,
+    (item, data) => {
+      item.documents.push(data);
+    }
+  );
+
+  return nextState;
+}
