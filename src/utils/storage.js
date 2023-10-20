d64faa978bcb6264f1e6dfc0de677bbf30b03b0b
ienrum <azang0506@gmail.com>
2023-10-20 13:55:02 +0900

    ✨ : storage.js 에 기존 함수들을 인터페이스화한 함수들 추가

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,3 +1,7 @@
+import { findDocumentFromTree, updateDocumentFromTree } from "./dataManager.js";
+const SIDE_BAR_KEY = "side-bar";
+const EDITING_POST_KEY = "editing-post";
+
 export const getStorageData = (key, init) => {
   const data = localStorage.getItem(key);
   if (data === null) {
@@ -7,9 +11,45 @@ export const getStorageData = (key, init) => {
 };
 
 export const setStorageData = (key, data) => {
-  localStorage(key, JSON.stringify(data));
+  localStorage.setItem(key, JSON.stringify(data));
 };
 
 export const removeStorageData = (key) => {
   localStorage.removeItem(key);
 };
+
+export const getSideBarData = () => {
+  return getStorageData(SIDE_BAR_KEY, []);
+};
+
+export const setSideBarData = (data) => {
+  setStorageData(SIDE_BAR_KEY, data);
+};
+
+export const removeSideBarData = () => {
+  removeStorageData(SIDE_BAR_KEY);
+};
+
+export const getEditingPostData = (id) => {
+  return getStorageData(EDITING_POST_KEY + id, {});
+};
+
+export const setEditingPostData = (id, data) => {
+  setStorageData(EDITING_POST_KEY + id, data);
+};
+
+export const removeEditingPostData = (id) => {
+  removeStorageData(EDITING_POST_KEY + id);
+};
+
+export function isDataInLocalStorage(id) {
+  const data = getStorageData(SIDE_BAR_KEY, null);
+  if (data === null) {
+    return false;
+  }
+  const result = findDocumentFromTree(data, id);
+  if (result) {
+    return true;
+  }
+  return false;
+}
