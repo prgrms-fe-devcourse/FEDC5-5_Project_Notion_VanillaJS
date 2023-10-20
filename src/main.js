91f45c6bd3fb7fcd9bc0654f0728ed719654bbe6
ienrum <azang0506@gmail.com>
2023-10-20 13:41:56 +0900

    🐛 : getIDs 가 없을경우 예외처리 수정

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import Header from "./components/Header.js";
 
 import { getData, getDatas, getIDs } from "./utils/fetchData.js";
 
+import { deleteDocuments } from "./utils/fetchData.js";
 const initialSideBarState = await getDatas();
-const initialId = getIDs(initialSideBarState);
+const initialId = getIDs(initialSideBarState)[0];
 let initialEditorState = { id: -1, title: "", content: "" };
-if (initialId.length > 0) {
+if (initialId && initialId.length > 0) {
   initialEditorState = await getData(initialId);
 }
 
