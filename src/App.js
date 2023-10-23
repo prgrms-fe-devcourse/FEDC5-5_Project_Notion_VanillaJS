afb7b4b15657055b563d6291e3c362084104592b
junly21 <jaejoon0721@ajou.ac.kr>
2023-10-23 09:02:34 +0900

    화면 좌측에 Root Tree렌더 기능구현. 버튼 클릭시  새 포스트 추가기능 구현.  DocumentTree에서 Document 클릭 시 해당  포스트로 이동 및 수정 가능.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,97 @@
 import RootContainer from "./Root/RootContainer.js";
 import DocumentContainer from "./Document/DocumentContainer.js";
+import { request } from "../library/api.js";
 
 export default function App({ $app }) {
   const $target = document.createElement("div");
   $target.id = "target";
 
   $app.appendChild($target);
-  const document_tree_dummy_data = [
-    {
-      id: 1, // Document id
-      title: "노션을 만들자", // Document title
-      documents: [
-        {
-          id: 2,
-          title: "블라블라",
-          documents: [
-            {
-              id: 3,
-              title: "함냐함냐",
-              documents: [],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 4,
-      title: "hello!",
-      documents: [],
-    },
-  ];
+  const initialState = {};
+  this.state = this.state;
 
-  const document_dummy_data = {
-    id: 1,
-    title: "노션을 만들자",
-    content: "즐거운 자바스크립트의 세계!",
-    documents: [
-      {
-        id: 2,
-        title: "",
-        createdAt: "",
-        updatedAt: "",
-      },
-    ],
-    createdAt: "",
-    updatedAt: "",
+  this.setState = (nextState) => {
+    this.state = nextState;
+    console.log("app.this.state", this.state);
+    rootContainer.setState(this.state);
   };
 
-  const initialState = document_tree_dummy_data;
+  //root에 뭐가 있는지 받아와서 setState해줌
+  //app의 initialState 데이터 형식과 일치
+  const fetchRoot = async () => {
+    const roots = await request(`/documents`);
+    this.setState(roots);
+  };
 
-  new RootContainer({
+  fetchRoot();
+
+  const fetchDoc = async (id) => {
+    const doc = await request(`/documents/${id}`);
+    return doc;
+  };
+
+  const rootContainer = new RootContainer({
     $target,
     initialState,
-    onClick: (documentId) => {
-      console.log(documentId);
+    onClick: async (documentId) => {
+      const doc = await fetchDoc(documentId);
+      documentContainer.setState(doc);
+      documentContainer.render();
     },
+    addNewDoc: async () => {
+      console.log("addneDoc시작");
+      const res = await fetchNewPost();
+      //{ res형식
+      // "id": 6,
+      // "title": "문서 제목",
+      // "createdAt": "생성된 날짜",
+      // "updatedAt": "수정된 날짜"
+      //}
+      //이 res를 root에 추가해줘
+      await fetchRoot(); //루트는 새로 렌더링해주고
+      //이제 fetchDoc해줘야지.
+      console.log(res); //res에 대해 출력됐고
+      const doc = await fetchDoc(res.id);
+      documentContainer.setState(doc);
+      documentContainer.render();
+    }, //Root에 새 문서추가해주면서 새 문서 작성 페이지로 이동해야대
+    //됐는데 내용이 null로 나오네
   });
 
-  new DocumentContainer({
+  const fetchNewPost = async () => {
+    const res = await request(`/documents`, {
+      method: "POST",
+      body: JSON.stringify({
+        title: "",
+        parent: "",
+      }),
+    });
+    return res;
+  };
+
+  const EditDoc = async (document) => {
+    const res = await request(`/documents/${document.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        title: `${document.title}`,
+        content: `${document.content}`,
+      }),
+    });
+  };
+
+  const documentContainer = new DocumentContainer({
     $target,
     initialState: {
       id: null,
       title: null,
-      content: null,
-      documents: null,
+      content: "",
+      documents: [],
       createAt: null,
       updatedAt: null,
+    },
+    onEdit: (document) => {
+      console.log("온에딧", document);
+      EditDoc(document);
     },
   });
 }
--- src/Document/DocumentContainer.js
+++ src/Document/DocumentContainer.js
@@ -1,7 +1,34 @@
-export default function DocumentContainer({ $target, initialState }) {
+export default function DocumentContainer({ $target, initialState, onEdit }) {
   const $page = document.createElement("div");
   $page.className = "DocumentContainer";
   $target.appendChild($page);
   $page.innerHTML = "DocumentContainer";
-  console.log(initialState);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+  };
+
+  let isInit = false;
+
+  this.render = () => {
+    if (this.state.title !== null) {
+      $page.innerHTML = `
+    <input type="text" name="title" style="width:93%;  height:5%; margin:20px; " value=${this.state.title}>
+    <textarea name="content" style="width:93%; height:80%; margin:20px;">${this.state.content}</textarea>
+`;
+      isInit = true;
+    }
+  };
+
+  $page.addEventListener("keyup", (e) => {
+    const name = e.target.name;
+    const nextState = {
+      ...this.state,
+      [name]: e.target.value,
+    };
+    this.setState(nextState);
+    onEdit(nextState);
+  });
 }
