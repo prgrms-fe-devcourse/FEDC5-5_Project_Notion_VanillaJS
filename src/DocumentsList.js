911c385c38a4e7d6a14a3f8168e6bea52c9c239b
hj9118 <hwl9118@gmail.com>
2023-10-24 22:08:47 +0900

    사이드 바 문서 표현 방식 리스트에서 토글로 변경

--- src/DocumentsList.js
+++ src/DocumentsList.js
@@ -19,58 +19,55 @@ export default function DocumentsList({ $target, onSelect }) {
     return documents
       .map(
         (doc) => `
-          <li data-id="${doc.id}">
-            ${doc.title}
-            <button class="add-child-button">+</button>
-            <button class="delete-button">-</button>
-            ${
-              doc.documents
-                ? `<ul>${renderDocuments(doc.documents)}</ul>`
-                : ''
-            }
-          </li>`
+          <details>
+            <summary data-id="${doc.id}">
+              ${doc.title}
+              <button class="add-child-button">+</button>
+              <button class="delete-button">-</button>
+            </summary>
+            ${doc.documents ? `<ul>${renderDocuments(doc.documents)}</ul>` : ''}
+          </details>`
       )
       .join('');
   };
-
+  
   this.render = () => {
     $documentsList.innerHTML = `
       <h1>Notion</h1>
       <ul>${renderDocuments(this.state)}</ul>`;
-
+  
     const $addChildButtons = document.querySelectorAll('.add-child-button');
     $addChildButtons.forEach((button) => {
       button.addEventListener('click', async (e) => {
         e.stopPropagation();
-
-        const { id } = e.target.closest('li').dataset;
-        const newDocument = await request('/documents', {
+  
+        const { id } = e.target.closest('summary').dataset;
+        await request('/documents', {
           method: 'POST',
           body: JSON.stringify({
             title: '제목 없음',
             parent: id,
           }),
         });
-
+  
         fetchDocuments();
       });
     });
-
+  
     const $deleteButtons = document.querySelectorAll('.delete-button');
     $deleteButtons.forEach((button) => {
       button.addEventListener('click', async (e) => {
         e.stopPropagation();
-
-        const { id } = e.target.closest('li').dataset;
+  
+        const { id } = e.target.closest('summary').dataset;
         await request(`/documents/${id}`, { method: 'DELETE' });
-
+  
         this.state = this.state.filter((doc) => doc.id !== id);
         fetchDocuments();
-        onSelect(null);
       });
     });
   };
-
+  
   const $addButton = document.createElement('button');
   $addButton.textContent = '새 글 추가';
   
@@ -82,22 +79,23 @@ export default function DocumentsList({ $target, onSelect }) {
         parent: null,
       }),
     });
-
-    fetchDocuments()
+  
+    fetchDocuments();
     onSelect(newDocument.id);
   });
-
+  
   $listContainer.appendChild($addButton);
   $target.appendChild($listContainer);
-
+  
   $documentsList.addEventListener('click', (e) => {
-    const $li = e.target.closest('li');
-
-    if ($li) {
-      const { id } = $li.dataset;
+    const $summary = e.target.closest('summary');
+  
+    if ($summary && !e.target.matches('button')) {
+      const { id } = $summary.dataset;
       onSelect(id);
     }
   });
+  
 
   const fetchDocuments = async () => {
     const docs = await request('/documents');
