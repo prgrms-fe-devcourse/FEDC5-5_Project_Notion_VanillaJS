6278a6f9d4c69ccf83bfe74f99ae7a4dcd621832
jonguk <sinjuk1@naver.com>
2023-10-24 12:41:00 +0900

    Feat: SideBarHeader 추가

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,8 +1,11 @@
+import SideBarHeader from "./SideBarHeader.js";
 import SideBarList from "./SideBarList.js";
 
 export default function SideBar({ $target, initialState }) {
     const $sideBar = document.createElement("div");
     $target.appendChild($sideBar);
+
+    new SideBarHeader({ $target: $sideBar });
 
     const sideBarList = new SideBarList({ $target: $sideBar, initialState });
     // console.log(Array.isArray(initialState));
--- src/SideBarHeader.js
+++ src/SideBarHeader.js
@@ -1,1 +1,9 @@
-export default function SideBarHeader() {}
+export default function SideBarHeader({ $target }) {
+    const Header = document.createElement("h3");
+    $target.appendChild(Header);
+
+    this.render = () => {
+        Header.innerText = `JongUk's Notion Home`;
+    };
+    this.render();
+}
