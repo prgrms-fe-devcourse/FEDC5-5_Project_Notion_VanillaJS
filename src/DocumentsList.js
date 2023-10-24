de50d5c9f667f27041bfd74108148078df7b6397
hj9118 <hwl9118@gmail.com>
2023-10-24 19:11:42 +0900

    사이드바 기본 기능 완성 - 기존 문서 추가 삭제 기능 - 새 문서 추가 - 레이아웃 설정 - 선택시 텍스트 수정 되도록 onSelect 설정

--- src/DocumentsList.js
+++ src/DocumentsList.js
@@ -1,8 +1,12 @@
 import { request } from './api.js';
 
 export default function DocumentsList({ $target, onSelect }) {
+  const $listContainer = document.createElement('div');
+  $listContainer.style.display = 'flex';
+  $listContainer.style.flexDirection = 'column';
+
   const $documentsList = document.createElement('div');
-  $target.appendChild($documentsList);
+  $listContainer.appendChild($documentsList);
 
   this.state = [];
 
@@ -11,28 +15,80 @@ export default function DocumentsList({ $target, onSelect }) {
     this.render();
   };
 
-  this.render = () => {
-    const renderDocuments = (documents) => {
-      return documents
-        .map(
-          (doc) => `
-            <li data-id="${doc.id}">
-              ${doc.title}
-              ${
-                doc.documents
-                  ? `<ul>${renderDocuments(doc.documents)}</ul>`
-                  : ''
-              }
-              </li>`
-              // 하위 문서 존재할 경우 재귀 호출)
-              )
-        .join('');
-    };
-
-    $documentsList.innerHTML = `
-    <h1>Notion</h1>
-    <ul>${renderDocuments(this.state)}</ul>`;
+  const renderDocuments = (documents) => {
+    return documents
+      .map(
+        (doc) => `
+          <li data-id="${doc.id}">
+            ${doc.title}
+            <button class="add-child-button">+</button>
+            <button class="delete-button">-</button>
+            ${
+              doc.documents
+                ? `<ul>${renderDocuments(doc.documents)}</ul>`
+                : ''
+            }
+          </li>`
+      )
+      .join('');
   };
+
+  this.render = () => {
+    $documentsList.innerHTML = `
+      <h1>Notion</h1>
+      <ul>${renderDocuments(this.state)}</ul>`;
+
+    const $addChildButtons = document.querySelectorAll('.add-child-button');
+    $addChildButtons.forEach((button) => {
+      button.addEventListener('click', async (e) => {
+        e.stopPropagation();
+
+        const { id } = e.target.closest('li').dataset;
+        const newDocument = await request('/documents', {
+          method: 'POST',
+          body: JSON.stringify({
+            title: '제목 없음',
+            parent: id,
+          }),
+        });
+
+        fetchDocuments();
+      });
+    });
+
+    const $deleteButtons = document.querySelectorAll('.delete-button');
+    $deleteButtons.forEach((button) => {
+      button.addEventListener('click', async (e) => {
+        e.stopPropagation();
+
+        const { id } = e.target.closest('li').dataset;
+        await request(`/documents/${id}`, { method: 'DELETE' });
+
+        this.state = this.state.filter((doc) => doc.id !== id);
+        fetchDocuments();
+        onSelect(null);
+      });
+    });
+  };
+
+  const $addButton = document.createElement('button');
+  $addButton.textContent = '새 글 추가';
+  
+  $addButton.addEventListener('click', async () => {
+    const newDocument = await request('/documents', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: '제목 없음',
+        parent: null,
+      }),
+    });
+
+    fetchDocuments()
+    onSelect(newDocument.id);
+  });
+
+  $listContainer.appendChild($addButton);
+  $target.appendChild($listContainer);
 
   $documentsList.addEventListener('click', (e) => {
     const $li = e.target.closest('li');
@@ -40,6 +96,7 @@ export default function DocumentsList({ $target, onSelect }) {
     if ($li) {
       const { id } = $li.dataset;
       onSelect(id);
+      console.log(id)
     }
   });
 
