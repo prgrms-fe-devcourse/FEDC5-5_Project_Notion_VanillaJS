a1194d48dec997929d42c5c6270b36e8f25dcf46
ienrum <azang0506@gmail.com>
2023-10-24 10:12:49 +0900

    🐛 : 에디터에 falsy 값 처리

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,64 +2,64 @@ import { isDataInLocalStorage } from "../utils/storage.js";
 import { getData, putData } from "../utils/fetchData.js";
 
 import {
-	getEditingPostData,
-	setEditingPostData,
-	removeEditingPostData,
+  getEditingPostData,
+  setEditingPostData,
+  removeEditingPostData,
 } from "../utils/storage.js";
 
 export default function Editor({ $target, initalState, setSideBarState }) {
-	const $div = document.createElement("div");
-	$div.id = "editor";
-	$target.appendChild($div);
+  const $div = document.createElement("div");
+  $div.id = "editor";
+  $target.appendChild($div);
 
-	this.state = initalState;
+  this.state = initalState;
 
-	this.setState = (nextState) => {
-		this.state = nextState;
-		this.render();
-	};
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
 
-	this.debounce = debounce(() => {
-		const body = {
-			title: this.state.title,
-			content: this.state.content,
-		};
-		putData(this.state.id, body).then((data) => {
-			setSideBarState(data);
-			removeEditingPostData(this.state.id);
-		});
-	}, 1000);
+  this.debounce = debounce(() => {
+    const body = {
+      title: this.state.title,
+      content: this.state.content,
+    };
+    putData(this.state.id, body).then((data) => {
+      setSideBarState(data);
+      removeEditingPostData(this.state.id);
+    });
+  }, 1000);
 
-	this.init = () => {
-		$div.addEventListener("keyup", (e) => {
-			if (e.target.tagName === "INPUT") {
-				this.state = { ...this.state, title: e.target.value };
-			} else if (e.target.tagName === "TEXTAREA") {
-				this.state = { ...this.state, content: e.target.value };
-			}
-			this.state.tempUpdatedAt = new Date().toISOString();
+  this.init = () => {
+    $div.addEventListener("keyup", (e) => {
+      if (e.target.tagName === "INPUT") {
+        this.state = { ...this.state, title: e.target.value };
+      } else if (e.target.tagName === "TEXTAREA") {
+        this.state = { ...this.state, content: e.target.value };
+      }
+      this.state.tempUpdatedAt = new Date().toISOString();
 
-			setEditingPostData(this.state.id, this.state);
-			this.debounce();
-		});
-	};
+      setEditingPostData(this.state.id, this.state);
+      this.debounce();
+    });
+  };
 
-	this.render = () => {
-		$div.innerHTML = `
-      <input type="text" value="${this.state.title}" />
-      <textarea>${this.state.content}</textarea>
+  this.render = () => {
+    $div.innerHTML = `
+      <input type="text" value="${this.state.title ? this.state.title : ""}" />
+      <textarea>${this.state.content ? this.state.content : ""}</textarea>
     `;
-	};
+  };
 
-	this.init();
+  this.init();
 }
 
 const debounce = (callback, delay) => {
-	let timer;
-	return () => {
-		clearTimeout(timer);
-		timer = setTimeout(() => {
-			callback();
-		}, delay);
-	};
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback();
+    }, delay);
+  };
 };
