cd07324c7b3ae2d3527f73e971212ee00ed8b407
hy219 <jhy_0219@daum.net>
2023-10-24 13:49:14 +0900

    Feat: 글 생성 url로 이동하는 버튼 생성(화면이동x)
    * 'New Post' 버튼 클릭 시, '/posts/new' url로 이동
    *  '/' -> '/posts/new'
    
    * 화면 이동 x

--- src/PostsPage.js
+++ src/PostsPage.js
@@ -1,5 +1,6 @@
 import PostList from "./PostList.js";
 import { request } from "./api.js";
+import { push } from "./router.js";
 
 export default function PostsPage({ $target }) {
   const $page = document.createElement("div");
@@ -9,6 +10,10 @@ export default function PostsPage({ $target }) {
   const $newPostButton = document.createElement("button");
   $newPostButton.textContent = "New Post";
   $page.appendChild($newPostButton);
+
+  $newPostButton.addEventListener("click", () => {
+    push("/posts/new");
+  });
 
   const fetchPosts = async () => {
     const posts = await request("/posts");
