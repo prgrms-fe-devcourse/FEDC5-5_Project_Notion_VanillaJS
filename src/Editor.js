d4abf48bc0554ed77cf77d4dfea050b83bf66f02
hj9118 <hwl9118@gmail.com>
2023-10-26 20:11:31 +0900

    flag함수 제거

--- src/Editor.js
+++ src/Editor.js
@@ -11,27 +11,24 @@ export default function Editor({
   $editor.style.display = 'flex';
   $editor.style.flexDirection = 'column';
 
-  let isInitialize = false;
   this.state = initialState;
   $target.appendChild($editor);
 
   this.setState = (nextState) => {
     this.state = nextState;
-    if (this.state) {
-      $editor.querySelector('[name=title]').value = this.state.title;
-      $editor.querySelector('[name=content]').value = this.state.content;
-    }
+    const titleElement = $editor.querySelector('[name=title]');
+    const contentElement = $editor.querySelector('[name=content]');
+    if (titleElement) titleElement.value = this.state.title;
+    if (contentElement) contentElement.value = this.state.content;
     this.render();
   };
 
-
   this.render = () => {
-    if (!isInitialize) {
+    if (!$editor.querySelector('[name=title]') && !$editor.querySelector('[name=content]')) {
       $editor.innerHTML = `
       <input type="text" name='title' class='title' placeholder='제목 없음' value="${this.state.title}" />
       <textarea name='content' class='content' placeholder='...'>${this.state.content}</textarea>
       `;
-      isInitialize = true;
     }
   };
 
