9a038e07294b8b828e2c0c91a389deac08746c28
hy219 <jhy_0219@daum.net>
2023-10-26 10:27:44 +0900

    Feat: document 생성 및 삭제 버튼 생성 & 삭제 기능 구현
    delete 버튼 클릭 시 해당 post 삭제

--- src/PostList.js
+++ src/PostList.js
@@ -1,4 +1,5 @@
 import { push } from "./router.js";
+import { request } from "./api.js";
 
 export default function PostList({ $target, initialState }) {
   const $postList = document.createElement("div");
@@ -12,20 +13,34 @@ export default function PostList({ $target, initialState }) {
   };
 
   this.render = () => {
-    $postList.innerHTML = `<ul>${this.state
-      .map((post) => `<li data-id="${post.id}">${post.title}</li>`)
-      .join("")}</ul>`;
+    $postList.innerHTML = `<ul>
+        ${this.state
+          .map(
+            (post) =>
+              `<li data-id="${post.id}">
+                ${post.title}
+                <button id="add">+</button>
+                <button id="delete">Delete</button>
+                
+            </li>`
+          )
+          .join("")}</ul>`;
   };
 
   //   this.render();
 
   $postList.addEventListener("click", (e) => {
     const $li = e.target.closest("li"); // postList내 클릭한 li 가져오기
-
+    const { id } = $li.dataset;
     if ($li) {
-      const { id } = $li.dataset;
+      push(`/documents/${id}`);
+    }
 
-      push(`/posts/${id}`);
+    const $buttonDelete = e.target.closest("li > button#delete");
+    if ($buttonDelete) {
+      request(`/documents/${id}`, {
+        method: "DELETE",
+      });
     }
   });
 }
